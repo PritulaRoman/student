4194e44c2dc484c67106371d3d387f55f192deda
Alona <alona.lykavska@gmail.com>
2019-02-14 23:11:17 +0200

    dynamic cards render refactoring

--- static/js/cards.js
+++ static/js/cards.js
@@ -1,36 +1,41 @@
-const getLinksCard = (cardLinks = []) => {
-	return cardLinks.map(({ title, href }) => {
-		const link = `<div class="card__text"><a href= ${href} >${title}</a></div>
-						`;
-		return link;
-	}).join('');
-}
+(function() {
+	const getLinksCard = (cardLinks = []) => {
+		return cardLinks.map(({ title, href }) => {
+			const link = `
+				<div class="card__text">
+					<a href="${href}">${title}</a>
+				</div>`;
+			return link;
+		}).join('');
+	}
 
+	const $boxCards = $('.js-box-cards');
 
-axios.get('https://jsonblob.com/api/1c066a98-2fd6-11e9-9080-df955f1091f2')
-  .then((response) => {
-  	const result = response.data.items;
+	axios.get('https://jsonblob.com/api/1c066a98-2fd6-11e9-9080-df955f1091f2')
+	  .then((response) => {
+	  	const result = response.data.items;
 
-  	const cards = result.map(({ title, description, links }) => {
-  		const linksCard = getLinksCard(links);
-  		
-		const card = `	<div class="card">
-							<div class="card__item card__img card__img--api"></div>
-							<div class="card__item card__title" name="api">${title}</div>
-							<div class="card__text">
-								${description}
-							</div>
-							${linksCard}
-						</div>
-					`;
-		return card;
-  	}).join('');
+	  	const cards = result.map(({ image, title, description, links }) => {
+	  		const linksCard = getLinksCard(links);
+	  		
+			const card = `
+				<div class="card">
+					<div class="card__item card__img" style="background-image: url('./static/images/cards/${ image }')"></div>
+					<div class="card__item card__title" name="api">${title}</div>
+					<div class="card__text">
+						${description}
+					</div>
+					${linksCard}
+				</div>
+			`;
+			return card;
+	  	}).join('');
 
-  	$('.box-cards').html(cards);
- 
-  })
+	  	$boxCards.html(cards);
+	  })
 
-  .catch((error) => {
-  	$('.box-cards').html("Sorry this API not available!!!");
-  })
+	  .catch((error) => {
+	  	$boxCards.html("Sorry this API not available!!!");
+	  });
+})();
 
