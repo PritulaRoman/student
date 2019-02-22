dbba18ec94e455d5e07861039c700e77d57ccf8c
Alona <alona.lykavska@gmail.com>
2019-02-22 13:45:58 +0200

    change code-apearence

--- src/js/active-btn.js
+++ src/js/active-btn.js
@@ -3,15 +3,15 @@ import $ from "jquery";
 let availableInedx = 0;
 
 const initBtns = () => {
-	$('.js-queue').click(function(event) {
-		const $this = $(this);
-		if(availableInedx === $this.data('index')) {
-			$this.addClass('box-btn__item--active');
-			availableInedx += 1;
-		}
+  $('.js-queue').click(function (event) {
+    const $this = $(this);
+    if (availableInedx === $this.data('index')) {
+      $this.addClass('box-btn__item--active');
+      availableInedx += 1;
+    }
 
-	});
-};	
+  });
+};
 
 export default initBtns;
 
--- src/js/cards.js
+++ src/js/cards.js
@@ -2,34 +2,34 @@ import $ from "jquery";
 import axios from "axios";
 
 const getLinksCard = (cardLinks = [], linkCssClass = '') => {
-	return cardLinks.map(({ title, href }) => {
-		const link = `
+  return cardLinks.map(({title, href}) => {
+    const link = `
 			<div class="card__text">
 				<a href="${href}" class="${linkCssClass}">${title}</a>
 			</div>`;
-		return link;
-	}).join('');
-}
+    return link;
+  }).join('');
+};
 
 export const buildCards = () => {
-	const $boxCards = $('.js-box-cards');
+  const $boxCards = $('.js-box-cards');
 
-	// Footer cards class
-	const $boxFooterCards = $('.js-box-cards-footer');
+  // Footer cards class
+  const $boxFooterCards = $('.js-box-cards-footer');
 
-	// Header cards
-	const $boxHeaderCards = $('.box-menu');
+  // Header cards
+  const $boxHeaderCards = $('.box-menu');
 
-	axios.get('https://jsonblob.com/api/1c066a98-2fd6-11e9-9080-df955f1091f2')
-	  .then((response) => {
-	  	const result = response.data.items;
+  axios.get('https://jsonblob.com/api/1c066a98-2fd6-11e9-9080-df955f1091f2')
+    .then((response) => {
+      const result = response.data.items;
 
-	  	const cards = result.map(({ image, title, description, links }) => {
-	  		const linksCard = getLinksCard(links || []);
-	  		
-			const card = `
+      const cards = result.map(({image, title, description, links}) => {
+        const linksCard = getLinksCard(links || []);
+
+        const card = `
 				<div class="card">
-					<div class="card__item card__img" style="background-image: url('./static/images/cards/${ image }')"></div>
+					<div class="card__item card__img" style="background-image: url('./static/images/cards/${image}')"></div>
 					<div class="card__item card__title" name="api">${title}</div>
 					<div class="card__text">
 						${description}
@@ -37,45 +37,45 @@ export const buildCards = () => {
 					${linksCard}
 				</div>
 			`;
-			return card;
-	  	}).join('');
+        return card;
+      }).join('');
 
-	  	$boxCards.html(cards);
-	  	return response;
-	  })
-	  // Footer cards
-	  .then((response) => {
-	  	const footerCardsInformation = response.data.results;
-	  	const footerCards = footerCardsInformation.map(({ title, links}) => {
-	  		const footerCardLinks = getLinksCard(links, 'card__text--footer');
-	  		const footerCard = `
+      $boxCards.html(cards);
+      return response;
+    })
+    // Footer cards
+    .then((response) => {
+      const footerCardsInformation = response.data.results;
+      const footerCards = footerCardsInformation.map(({title, links}) => {
+        const footerCardLinks = getLinksCard(links, 'card__text--footer');
+        const footerCard = `
 	  			<div class="card">
 					<div class="card__title card__title--footer">
 						${title}
 					</div>
 					${footerCardLinks}
 				</div>`;
-			return footerCard;
-	  	}).join('');
-	  	$boxFooterCards.html(footerCards);
-	  	return response;
-	  })
-	  .then((response) => {
-	  	const headerCardsData = response.data.header;
-	  	const headerCardes = headerCardsData.map(({ title, href, color}) => {
-	  		const headerCard = `
-	  			<div class="box-link" style='border-top: 8px solid ${ color }'>
-					<a href="${ href }" class="box-link__item" >${ title }</a>
+        return footerCard;
+      }).join('');
+      $boxFooterCards.html(footerCards);
+      return response;
+    })
+    .then((response) => {
+      const headerCardsData = response.data.header;
+      const headerCardes = headerCardsData.map(({title, href, color}) => {
+        const headerCard = `
+	  			<div class="box-link" style='border-top: 8px solid ${color}'>
+					<a href="${href}" class="box-link__item" >${title}</a>
 				</div>`;
-			return headerCard;
-	  	}).join('');
-	  	$boxHeaderCards.html(headerCardes);
-	  })
+        return headerCard;
+      }).join('');
+      $boxHeaderCards.html(headerCardes);
+    })
 
 
-	  .catch((error) => {
-	  	$boxCards.add($boxFooterCards).html("Sorry this API not available!!!");
-	  });
-}		
+    .catch((error) => {
+      $boxCards.add($boxFooterCards).html("Sorry this API not available!!!");
+    });
+};
 
 
