f3b1b6abb3a1ee6301e36919cbf41824600e0813
Alona <alona.lykavska@gmail.com>
2019-03-15 12:10:58 +0200

    Add class for loading active page. Add js logic for all active pages. Remove unnecessary function from file index. Add path for index page.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import initBtns from './js/active-btn';
 import { buildCards } from './js/cards';
 import initSearch from './js/active-search';
-import { buildMenuProducts } from './js/menu-products';
+
 
 buildCards();
 initBtns();
 initSearch();
-buildMenuProducts();
